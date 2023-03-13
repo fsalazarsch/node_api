6a2f275ef2c3727eaada696f67c60f00cf4bbfe6
Felipe Salazar Schlotterbeck <fsalazar.sch@gmail.com>
2023-03-13 00:57:04 -0300

    Add files via upload

---
+++ src/entity/Post.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Post {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    description: string
+
+}
